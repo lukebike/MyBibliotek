dd8d98d8cd7207c671a64a8a0e779fa00eb677a2
lukebike <lukesalem2002@gmail.com>
2025-08-03 14:19:51 +0200

    Added useSearch hook for reusability

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,1 +1,24 @@
 import { useMemo } from "react";
+import Fuse from "fuse.js";
+import type { IFuseOptions } from "fuse.js";
+import { useDebounce } from "./useDebounce";
+
+export function useSearch<T>(
+  data: T[],
+  searchTerm: string,
+  fuseOptions: IFuseOptions<T>
+) {
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const fuse = useMemo(() => {
+    return new Fuse(data, fuseOptions);
+  }, [data, fuseOptions]);
+
+  const filteredData = useMemo(() => {
+    return debouncedSearchTerm.trim()
+      ? fuse.search(debouncedSearchTerm).map((result) => result.item)
+      : data;
+  }, [fuse, debouncedSearchTerm, data]);
+
+  return filteredData;
+}
