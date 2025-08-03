455402e790a1d5099aebb8655490d077043113c0
lukebike <lukesalem2002@gmail.com>
2025-08-03 14:47:52 +0200

    Refactored GetBooks

--- src/routes/books/GetBooks.tsx
+++ src/routes/books/GetBooks.tsx
@@ -1,41 +1,22 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import api from "../../api";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
-import { Paper, TextField } from "@mui/material";
-import { useDebounce } from "../../hooks/useDebounce";
-import { DataGrid } from "@mui/x-data-grid";
 
-import Fuse from "fuse.js";
 import { useBookActionsMenu } from "../../hooks/useBookMenu";
 import { getBookColumns } from "../../components/GetBookColumns";
 
 import { useBookStore } from "../../store/bookStore";
+import { LoadingSpinner } from "../../components/LoadingSpinner";
+import { fuseConfigs } from "../../config/fuseConfigs";
+import { useSearch } from "../../hooks/useSearch";
+import { DataGridLayout } from "../../components/DataGridLayout";
 
 const GetBooks: React.FC = () => {
   const books = useBookStore((state) => state.books);
   const loading = useBookStore((state) => state.loading);
   const setBooks = useBookStore((state) => state.setBooks);
-  const [totalCount, setTotalCount] = useState(0);
   const setLoading = useBookStore((state) => state.setLoading);
-  const [paginationModel, setPaginationModel] = useState({
-    page: 0,
-    pageSize: 5,
-  });
 
   const [searchTerm, setSearchTerm] = useState("");
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-  const fuse = useMemo(() => {
-    const fuseOptions = {
-      threshold: 0.3,
-      keys: ["title", "publicationYear"],
-    };
-    return new Fuse(books, fuseOptions);
-  }, [books]);
 
   const { handleMenuOpen, BookMenu } = useBookActionsMenu();
   const columns = getBookColumns(handleMenuOpen);
@@ -44,90 +25,33 @@ const GetBooks: React.FC = () => {
     api
       .get("/books", {
         params: {
-          pageNumber: paginationModel.page,
-          pageSize: paginationModel.pageSize,
+          pageNumber: 0,
+          pageSize: 100,
         },
       })
       .then((res) => {
-        console.log(res.data);
         setBooks(res.data.books);
-        setTotalCount(res.data.totalCount);
       })
       .catch((err) => console.error("Error fetching books: ", err))
       .finally(() => setLoading(false));
-  }, [paginationModel, setBooks, setLoading]);
+  }, [setBooks, setLoading]);
 
-  if (loading)
-    return (
-      <Box
-        sx={{
-          display: "flex",
-          alignContent: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Typography variant="h4">Loading... </Typography> <CircularProgress />
-      </Box>
-    );
+  const filteredBooks = useSearch(books, searchTerm, fuseConfigs.books);
 
-  const filteredBooks = debouncedSearchTerm.trim()
-    ? fuse.search(debouncedSearchTerm).map((result) => result.item)
-    : books;
+  if (loading) return <LoadingSpinner />;
 
   return (
-    <Paper
-      elevation={3}
-      sx={{ p: 3, backgroundColor: "#121212", color: "#fff" }}
+    <DataGridLayout
+      title="Manage Books"
+      addUrl="/books/post"
+      searchTerm={searchTerm}
+      onSearchChange={setSearchTerm}
+      rows={filteredBooks}
+      columns={columns}
+      loading={loading}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          mb: 3,
-        }}
-      >
-        <Typography variant="h4">Manage Books</Typography>
-        <Button
-          href="/books/post"
-          variant="contained"
-          startIcon={<AddIcon />}
-          sx={{
-            backgroundColor: "grey",
-            "&:hover": {
-              backgroundColor: "cyan",
-            },
-          }}
-        >
-          Add Book
-        </Button>
-      </Box>
-      <TextField
-        fullWidth
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        sx={{ marginBottom: "10px" }}
-      />
-      <Box sx={{ height: "100%", width: "100%" }}>
-        <DataGrid
-          sx={{ backgroundColor: "#121212", color: "#fff" }}
-          rows={filteredBooks}
-          columns={columns}
-          loading={loading}
-          pageSizeOptions={[5, 10, 25]}
-          initialState={{
-            pagination: {
-              paginationModel: paginationModel,
-            },
-          }}
-          onPaginationModelChange={setPaginationModel}
-          paginationMode="server"
-          rowCount={totalCount}
-          disableRowSelectionOnClick
-        />
-      </Box>
       <BookMenu />
-    </Paper>
+    </DataGridLayout>
   );
 };
 
